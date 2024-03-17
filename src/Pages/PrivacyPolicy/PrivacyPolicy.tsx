399f3e0ec85333cc0e355bb18850aafca87c494f
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2024-03-17 03:41:14 +0200

    .

--- src/Pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/Pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -9,17 +9,17 @@ const PrivacyPolicy = () => {
       <div className="sticky left-0 top-0 z-[99] flex w-full items-center justify-around bg-black px-5 py-2">
         <div className="flex items-center justify-between gap-4">
           <img src={Logo} alt="logo" className="h-12 w-12 " />
-          <div className="text-3xl font-bold">{t("privacy_policy")}</div>
+          <div className="text-2xl font-bold xs:text-3xl">{t("privacy_policy")}</div>
         </div>
         <div>
           <button className={`${styles.coloredButton} !px-4`}>{t("download_pdf")}</button>
         </div>
       </div>
       <img src={cover} alt="cover" className="w-full" />
-      <div className="mb-5 flex h-14 w-full items-center justify-center bg-primary text-2xl font-bold text-black">{t("last_updated")}</div>
-      <div className="max-w-[75%]">
+      <div className="mb-5 flex h-14 w-full items-center justify-center bg-primary text-xl font-bold text-black xxs:text-2xl">{t("last_updated")}</div>
+      <div className="md:w-[75%]">
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("introduction")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary md:text-6xl">{t("introduction")}</div>
           <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} my-2 text-gray-400`}>{t("privacy_policy_text1")}</div>
           <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} my-2 text-gray-400`}>{t("privacy_policy_text2")}</div>
           <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} my-2 text-gray-400`}>{t("privacy_policy_text3")}</div>
@@ -27,7 +27,7 @@ const PrivacyPolicy = () => {
           <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} my-2 text-gray-400`}>{t("privacy_policy_text5")}</div>
         </div>
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("information_from_users")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary md:text-6xl">{t("information_from_users")}</div>
           <ul className={`list-disc ${i18next.language === "en" ? "pl-12" : "pr-12"}`}>
             <li className="my-2 text-gray-400">{t("information_from_users_text1")}</li>
             <li className="my-2 text-gray-400">{t("information_from_users_text2")}</li>
@@ -47,10 +47,10 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("information_from_cookies")}</div>
-          <div className="mb-4 text-4xl font-bold">{t("information_from_cookies_text1")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary md:text-6xl">{t("information_from_cookies")}</div>
+          <div className="mb-4 text-3xl font-bold md:text-4xl">{t("information_from_cookies_text1")}</div>
           <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("information_from_cookies_text11")}</div>
-          <div className="mb-4 text-4xl font-bold">{t("information_from_cookies_text2")}</div>
+          <div className="mb-4 text-3xl font-bold md:text-4xl">{t("information_from_cookies_text2")}</div>
           <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("information_from_cookies_text21")}</div>
           <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("information_from_cookies_text22")}</div>
           <ul className={`list-disc ${i18next.language === "en" ? "pl-12" : "pr-12"}`}>
@@ -62,7 +62,7 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("share_with_third_party")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary md:text-6xl">{t("share_with_third_party")}</div>
           <ul className={`list-disc ${i18next.language === "en" ? "pl-12" : "pr-12"}`}>
             <li className="my-2 text-gray-400">{t("share_with_third_party_text1")}</li>
             <li className="my-2 text-gray-400">{t("share_with_third_party_text2")}</li>
@@ -72,7 +72,7 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("what_do_we_do_with_your_personal_information")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary md:text-6xl">{t("what_do_we_do_with_your_personal_information")}</div>
           <ul className={`list-disc ${i18next.language === "en" ? "pl-12" : "pr-12"}`}>
             <li className="my-2 text-gray-400">{t("what_do_we_do_with_your_personal_information_text1")}</li>
             <li className="my-2 text-gray-400">{t("what_do_we_do_with_your_personal_information_text2")}</li>
@@ -82,17 +82,17 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("protect_information")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary md:text-6xl">{t("protect_information")}</div>
 
           <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} my-2 text-gray-400`}>{t("protect_information_text")}</div>
         </div>
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("control_over_your_information")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary md:text-6xl">{t("control_over_your_information")}</div>
 
           <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} my-2 text-gray-400`}>{t("control_over_your_information_text")}</div>
         </div>
       </div>
-      <div className="sticky bottom-0 left-0 z-[99] flex h-12 w-full items-center justify-center gap-2 bg-primary text-xl font-bold text-black">
+      <div className="sticky bottom-0 left-0 z-[99] flex h-12 w-full items-center justify-center gap-2 bg-primary text-xs font-bold text-black md:text-xl">
         {t("send_email_privacy")}
         <a href="mailto:contact@theline.social" className="underline">
           contact@theline.social
--- src/Pages/TermsOfService/TermsOfService.tsx
+++ src/Pages/TermsOfService/TermsOfService.tsx
@@ -10,21 +10,21 @@ const TermsOfService = () => {
   }
 
   return (
-    <div className="w-full overflow-y-scroll">
+    <div className="relative w-full overflow-y-scroll">
       <div className="sticky left-0 top-0 z-[99] flex w-full items-center justify-around bg-black px-5 py-2">
         <div className="flex items-center justify-between gap-4">
           <img src={Logo} alt="logo" className="h-12 w-12 " />
-          <div className="text-3xl font-bold">{t("terms_of_service")}</div>
+          <div className="xs:text-3xl text-2xl font-bold">{t("terms_of_service")}</div>
         </div>
         <div>
           <button className={`${styles.coloredButton} !px-4`}>{t("download_pdf")}</button>
         </div>
       </div>
       <img src={cover} alt="cover" className="w-full" />
-      <div className="mb-5 flex h-14 w-full items-center justify-center bg-primary text-2xl font-bold text-black">{t("last_updated")}</div>
-      <div className="max-w-[75%]">
+      <div className="mb-5 flex h-14 w-full items-center justify-center bg-primary text-xl font-bold text-black xxs:text-2xl">{t("last_updated")}</div>
+      <div className="md:w-[75%]">
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("introduction")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary xxs:text-6xl">{t("introduction")}</div>
           <div className="leading-7 text-gray-400">
             {getProcessedDescription(t("introduction_text"))
               .split(" ")
@@ -32,53 +32,53 @@ const TermsOfService = () => {
           </div>
         </div>
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("terms_and_definations")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary xxs:text-6xl">{t("terms_and_definations")}</div>
           <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} `}>
-            <div className="mb-4 text-4xl font-bold">{t("terms")}</div>
+            <div className="mb-4 text-3xl font-bold xxs:text-4xl">{t("terms")}</div>
             <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("terms_text")}</div>
-            <div className="mb-4 mt-4  text-4xl font-bold">{t("definitions")}</div>
+            <div className="mb-4 mt-4  text-3xl font-bold xxs:text-4xl">{t("definitions")}</div>
             <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} `}>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("definitions_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("you_your")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("you_your")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("you_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("we")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("we")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("we_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("administrator")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("administrator")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("administrator_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("policies")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("policies")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("policies_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("procedures")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("procedures")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("procedures_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("chat")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("chat")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("chat_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("precautions")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("precautions")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("precautions_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("enterprise")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("enterprise")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("enterprise_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("system")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("system")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("system_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("law")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("law")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("law_text")}</div>
 
-              <div className="my-2 text-2xl font-bold">{t("user_information")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("user_information")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("user_information_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("users")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("users")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("users_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("website")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("website")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("website_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("content_and_interactive_areas")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("content_and_interactive_areas")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("content_and_interactive_areas_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("automated_processing")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("automated_processing")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("automated_processing_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("third_party")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("third_party")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("third_party_text")}</div>
-              <div className="my-2 text-2xl font-bold">{t("terms_of_use")}</div>
+              <div className="my-2 text-xl font-bold xxs:text-2xl">{t("terms_of_use")}</div>
               <div className={`${i18next.language === "en" ? "pl-8" : "pr-8"} text-gray-400`}>{t("terms_of_use_text")}</div>
             </div>
           </div>
         </div>
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("general_terms")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary xxs:text-6xl">{t("general_terms")}</div>
           <ul className={`list-disc ${i18next.language === "en" ? "pl-12" : "pr-12"}`}>
             <li className="my-2 text-gray-400">{t("general_item1")}</li>
             <li className="my-2 text-gray-400">{t("general_item2")}</li>
@@ -93,7 +93,7 @@ const TermsOfService = () => {
           </ul>
         </div>
         <div className="flex flex-col gap-2 p-5 ">
-          <div className="mb-9 text-6xl font-bold text-primary">{t("electronic_terms")}</div>
+          <div className="mb-9 text-5xl font-bold text-primary xxs:text-6xl">{t("electronic_terms")}</div>
           <ul className={`list-disc ${i18next.language === "en" ? "pl-12" : "pr-12"}`}>
             <li className="my-2 text-gray-400">{t("electronic_item1")}</li>
             <li className="my-2 text-gray-400">{t("electronic_item2")}</li>
@@ -106,7 +106,7 @@ const TermsOfService = () => {
           </ul>
         </div>
       </div>
-      <div className="sticky bottom-0 left-0 z-[99] flex h-12 w-full items-center justify-center gap-2 bg-primary text-xl font-bold text-black">
+      <div className="sticky bottom-0 left-0 z-[99] flex h-12 w-full items-center justify-center gap-2 bg-primary text-xs font-bold text-black md:text-xl">
         {t("send_email")}
         <a href="mailto:contact@theline.social" className="underline">
           contact@theline.social
