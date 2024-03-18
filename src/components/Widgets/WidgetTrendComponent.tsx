08e223f799429db1dc7cc3aa12783793aa459ead
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2024-03-18 07:33:53 +0200

    Update WidgetTrendComponent.tsx

--- src/components/Widgets/WidgetTrendComponent.tsx
+++ src/components/Widgets/WidgetTrendComponent.tsx
@@ -2,6 +2,14 @@ import React from "react"
 import { t } from "i18next"
 
 const WidgetsTrendComponent = ({ index, name, numberOfPosts }: { index: number; name: string; numberOfPosts: number }) => {
+  function isArabicChar(char: string): boolean {
+    return /[\u0600-\u06FF]/.test(char)
+  }
+
+  function hasArabicChars(text: string): boolean {
+    return text.split("").some(isArabicChar)
+  }
+
   return (
     <div
       className="flex  w-full cursor-pointer flex-col justify-between p-3 hover:bg-lightHover dark:hover:bg-[#292d34]"
@@ -14,7 +22,7 @@ const WidgetsTrendComponent = ({ index, name, numberOfPosts }: { index: number; 
         {index}. {t("in_trending")}
       </div>
       <div dir="ltr" className="self-end">
-        #{name}
+        {`${hasArabicChars(name) ? name + "#" : "#" + name}`}
       </div>
       <div className="text-start text-xs text-gray-500">
         {numberOfPosts} {t("posts")}
